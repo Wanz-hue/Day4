887258e4bfb51e1253e61bf784900f64d132d8df
sena <hargiantofebrisena@gmail.com>
2022-03-24 20:44:30 +0700

    initial

--- js/project.js
+++ js/project.js
@@ -1,16 +1,22 @@
 let blogs = [];
+let choice = [];
+
+function choices(value) {
+  console.log(value);
+}
+
 function addBlog(event) {
   event.preventDefault();
   let project = document.getElementById("input-project").value;
   let description = document.getElementById("input-description").value;
   let image = document.getElementById("input-image").files;
+  image = URL.createObjectURL(image[0]);
   let blog = {
     project: project,
     description: description,
     image: image,
   };
   blogs.push(blog);
-  console.log(blog);
   renderBlog();
 }
 
@@ -19,6 +25,11 @@ function renderBlog() {
   for (let i in blogs) {
     document.getElementById("blogs").innerHTML += `
     <div class="box-blog1">
+      <img
+        src="${blogs[i].image}"
+        class="image"
+        alt=""
+      /> 
       <h3>${blogs[i].project}</h3>
       <p>
         ${blogs[i].description}
